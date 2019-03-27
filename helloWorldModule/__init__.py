8b6dce127677862043dffbdba369500cf64452cc
Finomnis <martin.h.stumpf@gmail.com>
2019-03-27 11:40:25 +0100

    made chdir more robust

--- helloWorldModule/__init__.py
+++ helloWorldModule/__init__.py
@@ -4,23 +4,28 @@ import os as __os
 import glob as __glob
 
 def __build():
-    cPath = __os.path.dirname(__os.path.realpath(__file__))
-    print("Building library '" + __os.path.basename(cPath) + "' ...")
-
     pwd_bup = __os.getcwd()
-    __os.chdir(cPath)
 
-    sources = __glob.glob('__src/*.c')
-    #print("   Sources:", sources)
+    try:
+        cPath = __os.path.dirname(__os.path.realpath(__file__))
+        print("Building library '" + __os.path.basename(cPath) + "' ...")
+    
+        __os.chdir(cPath)
+    
+        sources = __glob.glob('__src/*.c')
+        #print("   Sources:", sources)
+    
+        module1 = __extension('__lib',
+                              sources=sources)
 
-    module1 = __extension('__lib',
-                          sources=sources)
+        raise ValueError("Bad!")
 
-    __setup(name='__lib',
-            ext_modules=[module1],
-            script_args=["build_ext", "--inplace"])
+        __setup(name='__lib',
+                ext_modules=[module1],
+                script_args=["build_ext", "--inplace"])
 
-    __os.chdir(pwd_bup)
+    finally:
+        __os.chdir(pwd_bup)
 
 __build()
 
