b780b74ba1f1caaf4b05d88a74115324b824744b
Finomnis <martin.h.stumpf@gmail.com>
2019-03-27 11:42:38 +0100

    changed variable names, removed accidentally committed raise

--- helloWorldModule/__init__.py
+++ helloWorldModule/__init__.py
@@ -4,13 +4,13 @@ import os as __os
 import glob as __glob
 
 def __build():
-    pwd_bup = __os.getcwd()
+    cwd_bup = __os.getcwd()
 
     try:
-        cPath = __os.path.dirname(__os.path.realpath(__file__))
-        print("Building library '" + __os.path.basename(cPath) + "' ...")
+        modulePath = __os.path.dirname(__os.path.realpath(__file__))
+        print("Building library '" + __os.path.basename(modulePath) + "' ...")
     
-        __os.chdir(cPath)
+        __os.chdir(modulePath)
     
         sources = __glob.glob('__src/*.c')
         #print("   Sources:", sources)
@@ -18,14 +18,12 @@ def __build():
         module1 = __extension('__lib',
                               sources=sources)
 
-        raise ValueError("Bad!")
-
         __setup(name='__lib',
                 ext_modules=[module1],
                 script_args=["build_ext", "--inplace"])
 
     finally:
-        __os.chdir(pwd_bup)
+        __os.chdir(cwd_bup)
 
 __build()
 
