9226e27783bb61e794a7bae1216d60aa0f0cc954
Martin Stumpf <Finomnis@users.noreply.github.com>
2019-03-28 21:14:37 +0100

    Update wrappers.py

--- helloWorldCppModule/__src/wrappers.py
+++ helloWorldCppModule/__src/wrappers.py
@@ -1,10 +1,10 @@
 from .. import __lib
 
-# This file contains python wrappers for our C functions.
+# This file contains python wrappers for our C++ functions.
 # The whole purpose of that is to make it easier for
 # auto-completions to know our function definitions.
 
-# __lib is the compiled library containing our c functions.
+# __lib is the compiled library containing our c++ functions.
 
 
 def hello(name):
