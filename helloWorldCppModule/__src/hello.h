444ae3fc84a5f927ef49e73220ab6d8c224a10bf
Martin Stumpf <Finomnis@users.noreply.github.com>
2019-03-29 10:02:05 +0100

    Update hello.h

--- helloWorldCppModule/__src/hello.h
+++ helloWorldCppModule/__src/hello.h
@@ -1,7 +1,7 @@
 #ifndef MYPYBINDMODULE_HELLO_H
 #define MYPYBINDMODULE_HELLO_H
 
-// Always include <pybind11/pybind11.h> in the very first line of all header files and module.c
+// Always include <pybind11/pybind11.h> in the very first line of all header files and module.cpp
 // In general, make sure that you include pybind11.h BEFORE all other header files.
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
