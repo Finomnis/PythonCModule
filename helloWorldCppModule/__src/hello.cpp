446fb664927d3596bf1190ca124df0b568b69d57
Martin Stumpf <Finomnis@users.noreply.github.com>
2019-03-29 10:01:42 +0100

    Update hello.cpp

--- helloWorldCppModule/__src/hello.cpp
+++ helloWorldCppModule/__src/hello.cpp
@@ -1,4 +1,4 @@
-// In all C files, include its corresponding header file in the very first line.
+// In all C++ files, include its corresponding header file in the very first line.
 // No need to include <pybind11.h> as we did that already in the header file.
 // Just make sure that <pybind11.h> is included BEFORE any other header file.
 #include "hello.h"
