7393cd11d3d35133105cc37884b9dc5955d73602
Finomnis <martin.h.stumpf@gmail.com>
2019-05-08 16:48:07 +0200

    simplified __init__.py from c code

--- helloWorldModule/__init__.py
+++ helloWorldModule/__init__.py
@@ -1,29 +1,29 @@
-from distutils.core import setup as __setup
-from distutils.core import Extension as __extension
-import os as __os
-import glob as __glob
-
 def __build():
-    cwd_bup = __os.getcwd()
+    from distutils.core import setup
+    from distutils.core import Extension
+    import os
+    import glob
+
+    cwd_bup = os.getcwd()
 
     try:
-        modulePath = __os.path.dirname(__os.path.realpath(__file__))
-        print("Building library '" + __os.path.basename(modulePath) + "' ...")
+        modulePath = os.path.dirname(os.path.realpath(__file__))
+        print("Building library '" + os.path.basename(modulePath) + "' ...")
     
-        __os.chdir(modulePath)
+        os.chdir(modulePath)
     
-        sources = __glob.glob('__src/**/*.c', recursive=True)
+        sources = glob.glob('__src/**/*.c', recursive=True)
         #print("   Sources:", sources)
     
-        module1 = __extension('__lib',
+        module1 = Extension('__lib',
                               sources=sources)
 
-        __setup(name='__lib',
+        setup(name='__lib',
                 ext_modules=[module1],
                 script_args=["-q", "build_ext", "--inplace"])
 
     finally:
-        __os.chdir(cwd_bup)
+        os.chdir(cwd_bup)
 
 __build()
 
